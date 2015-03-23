7463bb8e8c5691c3596d0198428cf268ca19ce4c
Dustin <dustinmarx@gmail.com>
2015-03-22 18:25:05 -0600

    Create AlbumConstants.java

---
+++ src/dustin/jpa/AlbumConstants.java
@@ -0,0 +1,8 @@
+package dustin.jpa;
+
+public class AlbumConstants 
+{
+   public static final String ALBUM_PU = "Album_Unit";
+   
+   public static final int INITIAL_BUFFER_SIZE_TO_STRING = 100;
+}
